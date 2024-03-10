b00c2a49952a0ce02518d34bac8d839d57aa2aca
Cengiz Ulusoy <Cengizism@users.noreply.github.com>
2024-03-10 13:33:46 +0100

    Ready for custom silder

--- pages/p/[photoId].tsx
+++ pages/p/[photoId].tsx
@@ -17,11 +17,11 @@ const Home: NextPage = ({ currentPhoto }: { currentPhoto: ImageProps }) => {
   return (
     <>
       <Head>
-        <title>Next.js Conf 2022 Photos</title>
+        <title>Bilal slide</title>
         <meta property="og:image" content={currentPhotoUrl} />
         <meta name="twitter:image" content={currentPhotoUrl} />
       </Head>
-      <main className="mx-auto max-w-[1960px] p-4">
+      <main className="mx-auto h-full w-full max-w-[1960px] p-4">
         <Carousel currentPhoto={currentPhoto} index={index} />
       </main>
     </>
@@ -47,7 +47,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
   }
 
   const currentPhoto = reducedResults.find(
-    (img) => img.id === Number(context.params.photoId),
+    (img) => img.id === Number(context.params.photoId)
   );
   currentPhoto.blurDataUrl = await getBase64ImageUrl(currentPhoto);
 
