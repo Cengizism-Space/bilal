b9276ece27167c85bf520e36542b17e322bd23f4
Cengiz Ulusoy <Cengizism@users.noreply.github.com>
2024-03-10 08:21:34 +0100

    Cleaned up a bit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,19 +8,19 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <meta
             name="description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="Hellevoetsluis HDV Eyüp Sultan Camii TV mededelingen en activiteiten"
           />
           <meta property="og:site_name" content="nextjsconf-pics.vercel.app" />
           <meta
             property="og:description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="Hellevoetsluis HDV Eyüp Sultan Camii TV mededelingen en activiteiten"
           />
           <meta property="og:title" content="Next.js Conf 2022 Pictures" />
           <meta name="twitter:card" content="summary_large_image" />
           <meta name="twitter:title" content="Next.js Conf 2022 Pictures" />
           <meta
             name="twitter:description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="Hellevoetsluis HDV Eyüp Sultan Camii TV mededelingen en activiteiten"
           />
         </Head>
         <body className="bg-black antialiased">
